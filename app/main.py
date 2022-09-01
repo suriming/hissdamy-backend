e369b24656bdf3307003ebcd3b521be2ba930b18
suriming <soolim520@gmail.com>
2022-09-01 13:02:05 +0900

    feat: init

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from typing import Optional
+from fastapi import FastAPI
+from mangum import Mangum
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+handler = Mangum(app)
