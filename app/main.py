0a680d265093d45a8fe480f85933d30abf223793
suriming <soolim520@gmail.com>
2022-10-16 22:57:38 +0900

    train

--- app/main.py
+++ app/main.py
@@ -2,18 +2,27 @@ from typing import Optional, Union
 from fastapi import FastAPI
 from mangum import Mangum
 import pickle
+from matplotlib.pyplot import sca
 import pandas as pd
 from pydantic import BaseModel
 import numpy as np
-import catboost
+from catboost import CatBoostClassifier, Pool
 
 from sklearn.preprocessing import StandardScaler
-from fastapi.middleware.cors import CORSMiddleware
+# from fastapi.middleware.cors import CORSMiddleware
+
+from sklearn.model_selection import train_test_split
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
+import joblib
+from sklearn.metrics import accuracy_score
+
+
 
 app = FastAPI()
 
 origins = [
-    "http://localhost:3000"
+    "http://localhost:3000", "http://0.0.0.0:3000", "http://127.0.0.1:8000", "http://localhost:3000/complete"
 ]
 
 app.add_middleware(
@@ -23,6 +32,20 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# middleware =[Middleware(CORSMiddleware, allow_or)]
+
+# origins = [
+#     "http://localhost:3000", "http://0.0.0.0:3000"
+# ]
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 class KeypointsItem(BaseModel):
     x: float
     y: float
@@ -33,8 +56,13 @@ class KeypointsItem(BaseModel):
 class Scoringitem(BaseModel):
     keypoints: Union[list[KeypointsItem], None] = None
 
+class Scoringitem_train(BaseModel):
+    keypoints: Union[list[KeypointsItem], None] = None
+    label: int
+
 class Trainitem(BaseModel):
-    frame: Union[list[Scoringitem], None] = None
+    frame: Union[list[Scoringitem_train], None] = None
+    # frame: Union[Scoringitem, None] = None
 
 with open('cat_weight.pkl', 'rb') as f:
     model = pickle.load(f)
@@ -49,8 +77,46 @@ def read_item(item_id: int, q: Optional[str] = None):
 
 @app.post('/train/')
 def train(item:Trainitem):
-    
-    return item
+    data = pd.DataFrame(columns=['left_eye_inner_x', 'left_eye_x', 'left_eye_outer_x', 
+                             'right_eye_inner_x', 'right_eye_x', 'right_eye_outer_x', 
+                             'nose_x','mouth_left_x','mouth_right_x','ans'])
+    for i in range(len(item.frame)):
+        data.loc[i] = [item.frame[i].keypoints[1].x, item.frame[i].keypoints[2].x, item.frame[i].keypoints[3].x, 
+                    item.frame[i].keypoints[4].x, item.frame[i].keypoints[5].x, item.frame[i].keypoints[9].x,
+                    item.frame[i].keypoints[0].x, item.frame[i].keypoints[9].x, item.frame[i].keypoints[10].x, item.frame[i].label]
+
+    new_data = pd.DataFrame(columns=['0_2_dist_x', '0_5_dist_x', '0_2_5_diff_x', '1_4_dist_x', '0_9_10_diff_x', '0_9_10_ratio_x'] )
+    new_data['0_2_dist_x'] = abs(data['left_eye_x'] - data['nose_x'])
+    new_data['0_5_dist_x'] = abs(data['right_eye_x'] - data['nose_x'])
+    new_data['0_2_5_diff_x'] = new_data['0_5_dist_x'] - new_data['0_2_dist_x']
+    new_data['1_4_dist_x'] = abs(data['right_eye_inner_x'] - data['left_eye_inner_x'])
+    new_data['0_9_10_diff_x'] = data['nose_x'] - ((data['mouth_left_x'] + data['mouth_right_x']) / 2)
+    new_data['0_9_10_ratio_x'] = data['nose_x'] / ((data['mouth_left_x'] + data['mouth_right_x']) / 2)
+    new_data['ans'] = data['ans']
+
+    X = new_data.drop(['ans'], axis=1)
+    y = new_data[['ans']]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
+    X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=0.5, random_state=42)
+
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+
+    X_train = scaler.transform(X_train)
+    X_test = scaler.transform(X_test)
+    X_val = scaler.transform(X_val)
+
+    cat_clf = CatBoostClassifier(iterations=50,random_seed=42, max_bin=8)
+    cat_clf.fit(X_train, y_train)
+    y_pred = cat_clf.predict(X_val)
+
+    filename = 'cat_weight.pkl'
+    joblib.dump(cat_clf, filename)
+
+    scr = accuracy_score(y_val, y_pred)
+
+    return int(scr)
 
 
 @app.post('/test/')
